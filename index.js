7c9cb5690083a1fecdb5918180ea823c73a4715c
alvaroid01 <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>
2024-12-22 02:08:56 +0100

    Added style

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded',(evt=>{
-
-
-
-  document.body.textContent = 'Hola mundo!'
-
+  const content = document.querySelectorAll('.content-right div')
+  content.forEach((e)=>{
+    if(id === section) e.style.display = 'flex'
+   else  e.style.display = 'none'
+ })
 }))
+
+  function goToIndex(params) {
+   document.querySelector('.content-left').textContent = 'Fuck off' 
+  }
+  function renderSection(section){
+    document.querySelector('.current-path').textContent = `${section}.html`
+    const content = document.querySelectorAll('.content-right div')
+
+    content.forEach((e)=>{
+       const id = e.getAttribute('id')
+       if(id === section) e.style.display = 'flex'
+      else  e.style.display = 'none'
+    })
+  }
